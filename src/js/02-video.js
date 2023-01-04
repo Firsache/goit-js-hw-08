5e68131c6a779aa2cd31306266001b4c4220db73
Firsova Liubov <firsova.lyub@gmail.com>
2023-01-04 21:35:51 +0200

    added lib to task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+// import Vimeo from '@vimeo/player'
+// import throttle from 'lodash.throttle'
+
+
+// const player = new Vimeo.Player(iframe);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const currentTime = player.on('timeupdate', function (data) {
+    console.log(data.seconds);
+    return data.seconds;
+});
+
+// document.addEventListener(
+// 'timeupdate',
+// throttle(() => {
+    
+// },100)
+// )
+
+// localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime));
 
