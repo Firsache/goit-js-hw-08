1f23252cd5354479221d5a04f7bfbb58e58d4677
Firsova Liubov <firsova.lyub@gmail.com>
2023-01-05 21:39:18 +0200

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    messageTextArea: document.querySelector('.feedback-form textarea')
+}
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+populateFormInput();
+let formInfo = {email:'',message:''};
+function onFormInput(evt) {
+    
+    let form = evt.target;
+    
+    if (form.name === 'email') {
+        formInfo.email = form.value;
+    } else if (form.name === 'message') {
+        formInfo.message = form.value;
+    }
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formInfo));
+}
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+
+    // form.elements.message.value
+    // localStorage.getItem(LOCALSTORAGE_KEY) || "";
+}
+function populateFormInput() {
+    const savedUserInfo = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    
+    if (savedUserInfo.email) {
+        refs.form[0].value = savedUserInfo.email;
+    }
+    if (savedUserInfo.message) {
+        refs.form[1].value = savedUserInfo.message;
+    }
+}
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
