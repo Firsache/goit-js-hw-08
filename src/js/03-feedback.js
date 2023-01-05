a60c8f447e0d93cb67664b9bccc2a0c66685f1c0
Firsova Liubov <firsova.lyub@gmail.com>
2023-01-05 22:04:28 +0200

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,26 @@
 import throttle from "lodash.throttle";
 
 const refs = {
-    form: document.querySelector('.feedback-form'),
+    formEL: document.querySelector('.feedback-form'),
     messageTextArea: document.querySelector('.feedback-form textarea')
 }
 const LOCALSTORAGE_KEY = "feedback-form-state";
 
 populateFormInput();
-let formInfo = {email:'',message:''};
+let formData = {email:'',message:''};
 function onFormInput(evt) {
     
     let form = evt.target;
-    
-    if (form.name === 'email') {
-        formInfo.email = form.value;
-    } else if (form.name === 'message') {
-        formInfo.message = form.value;
-    }
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formInfo));
+    formData[form.name] = form.value;
+    // ====================================================================
+    //     my first idea is below
+    // 
+    // if (form.name === 'email') {
+    //     formInfo.email = form.value;
+    // } else if (form.name === 'message') {
+    //     formInfo.message = form.value;
+    // }
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 }
 function onFormSubmit(evt) {
     evt.preventDefault();
@@ -29,13 +32,21 @@ function onFormSubmit(evt) {
 }
 function populateFormInput() {
     const savedUserInfo = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-    
-    if (savedUserInfo.email) {
-        refs.form[0].value = savedUserInfo.email;
+    let formElement = refs.formEL.elements;
+    // console.dir(refs.form);
+    if (savedUserInfo) {
+        formElement.email.value = savedUserInfo.email || '';
+        formElement.message.value = savedUserInfo.message||'';
     }
-    if (savedUserInfo.message) {
-        refs.form[1].value = savedUserInfo.message;
-    }
+    // ====================================================================
+    //     my first idea is below
+    // 
+    // if (savedUserInfo.email) {
+    //     refs.form[0].value = savedUserInfo.email;
+    // }
+    // if (savedUserInfo.message) {
+    //     refs.form[1].value = savedUserInfo.message;
+    // }
 }
-refs.form.addEventListener('input', throttle(onFormInput, 500));
-refs.form.addEventListener('submit', onFormSubmit);
+refs.formEL.addEventListener('input', throttle(onFormInput, 500));
+refs.formEL.addEventListener('submit', onFormSubmit);
